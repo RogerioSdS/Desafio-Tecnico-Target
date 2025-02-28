e75cf3afb322a71568e5c3c9dca92b66642ae205
Rogerio Soares <rogerio.soares@frenet.com.br>
2025-02-28 12:35:27 -0300

    Criando soluções para a questão 5

---
+++ Questao5_Inversor_String/Questao5_Inversor_String/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Questao5_Inversor_String
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Digite uma string para inverter: ");
+            string input = Console.ReadLine();
+
+            string invertedString = InverterString(input);
+
+            Console.WriteLine($"A string invertida é: {invertedString}");
+        }
+
+        static string InverterString(string str)
+        {
+            string inverted = "";  
+
+            for (int i = str.Length - 1; i >= 0; i--)
+            {
+                inverted += str[i];  
+            }
+
+            return inverted;
+        }
+    }
+}
